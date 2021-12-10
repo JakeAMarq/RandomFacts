d5aba7aeb0e5aa0dac9fd5a3dbfebbad8799654b
JakeAMarq <jacobmarquardt1@gmail.com>
2021-12-10 00:10:01 -0800

    In MainActivity:     - Removed all the logic that was there for testing FactRepository and its use cases     - Added a nav host with two screens:         - Fact list screen         - Year fact screen that accepts a year parameter

--- app/src/main/java/com/jakem/randomfacts/MainActivity.kt
+++ app/src/main/java/com/jakem/randomfacts/MainActivity.kt
@@ -1,79 +1,78 @@
 package com.jakem.randomfacts
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.clickable
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.lifecycleScope
-import com.jakem.randomfacts.core.util.Resource
-import com.jakem.randomfacts.feature_facts.domain.repository.FactRepository
-import com.jakem.randomfacts.feature_facts.domain.use_cases.GetNumberFactsUseCase
-import com.jakem.randomfacts.feature_facts.domain.use_cases.GetYearFactUseCase
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.jakem.randomfacts.RandomFactsScreen.FactList
+import com.jakem.randomfacts.RandomFactsScreen.YearFact
 import com.jakem.randomfacts.ui.theme.RandomFactsTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collect
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    @Inject
-    lateinit var getNumberFactsUseCase: GetNumberFactsUseCase
-    @Inject
-    lateinit var getYearFactUseCase: GetYearFactUseCase
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        lifecycleScope.launchWhenCreated {
-
-            getNumberFactsUseCase(1, 50).collect { result ->
-                Log.d("MainActivity", "NumberFacts:")
-                when (result) {
-                    is Resource.Loading -> Log.d("MainActivity", "\tLoading")
-                    is Resource.Error -> Log.d("MainActivity", "\tError")
-                    is Resource.Success -> Log.d("MainActivity", "\tSuccess")
-                }
-                Log.d("MainActivity", "\tMessage: " + result.message)
-                Log.d("MainActivity", "\tData: " + result.data)
-            }
-
-            getYearFactUseCase(1998).collect { result ->
-                when (result) {
-                    is Resource.Loading -> Log.d("MainActivity", "loading year facts")
-                    is Resource.Error -> Log.d("MainActivity", result.message ?: "Error")
-                    is Resource.Success -> Log.d("MainActivity", result.data.toString())
-                }
-            }
-
-        }
-
-
         setContent {
-            RandomFactsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            RandomFactsApplication()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun RandomFactsApplication() {
+    RandomFactsTheme {
+        val navController = rememberNavController()
+
+        Scaffold {
+            RandomFactsNavHost(navController = navController)
+        }
+
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    RandomFactsTheme {
-        Greeting("Android")
+fun RandomFactsNavHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = FactList.name,
+        modifier = modifier
+    ) {
+        composable(FactList.name) {
+            Text(
+                text = "FactList screen",
+                modifier = Modifier.clickable {
+                    navController.navigate("${YearFact.name}/1998")
+                }
+            )
+        }
+        composable(
+            route = "${YearFact.name}/{year}",
+            arguments = listOf(
+                navArgument("year") {
+                    type = NavType.IntType
+                }
+            )
+        ) { entry ->
+            Text("Year screen: ${entry.arguments?.getInt("year")}")
+        }
     }
+}
+
+enum class RandomFactsScreen {
+    FactList, YearFact
 }
